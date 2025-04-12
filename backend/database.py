71039dde2491dab5de2bb604bed88b86d68b395d
Isaac Zarnke <Izarnke5568@conestogac.on.ca>
2025-04-11 23:37:58 -0400

    Changed Route model and document to include starting and ending coordinates.
    Added Models for historcal_data, roads, routes, snow_conditions
    
    Added CRUD functionaliy for historical_data, roads, routes, snow_conditions

--- backend/database.py
+++ backend/database.py
@@ -37,3 +37,98 @@ def delete_driver_by_id(id: str):
     delete_result = drivers_collection.delete_one({"_id:": id})
     if delete_result:
         return delete_result
+
+        def insert_historical_data(historical_data: models.HistoricalData):
+    collection = db["historical_data"]
+    result = collection.insert_one(historical_data.to_dict())
+    return str(result.inserted_id)
+
+# Historical Data Functions:
+def get_historical_data_by_road_id(road_id: str):
+    collection = db["historical_data"]
+    data = collection.find_one({"historical_information.road_id": road_id})
+    if data:
+        return models.HistoricalData.from_dict(data)
+
+def get_historical_data_by_id(id: str):
+    collection = db["historical_data"]
+    data = collection.find_one({"_id": ObjectId(id)})
+    if data:
+        return models.HistoricalData.from_dict(data)
+
+def delete_historical_data_by_id(id: str):
+    collection = db["historical_data"]
+    result = collection.delete_one({"_id": ObjectId(id)})
+    return result
+
+
+# Road Functions:
+
+def insert_road(road: models.Road):
+    collection = db["roads"]
+    result = collection.insert_one(road.to_dict())
+    return str(result.inserted_id)
+
+def get_road_by_name(name: str):
+    collection = db["roads"]
+    data = collection.find_one({"route_information.road_name": name})
+    if data:
+        return models.Road.from_dict(data)
+
+def get_road_by_id(id: str):
+    collection = db["roads"]
+    data = collection.find_one({"_id": ObjectId(id)})
+    if data:
+        return models.Road.from_dict(data)
+
+def delete_road_by_id(id: str):
+    collection = db["roads"]
+    result = collection.delete_one({"_id": ObjectId(id)})
+    return result
+
+# Route Functions:
+def insert_route(route: models.Route):
+    collection = db["routes"]
+    result = collection.insert_one(route.to_dict())
+    return str(result.inserted_id)
+
+def get_route_by_driver_id(driver_id: str):
+    collection = db["routes"]
+    data = collection.find_one({"route_information.driver_id": driver_id})
+    if data:
+        return models.Route.from_dict(data)
+
+def get_route_by_id(id: str):
+    collection = db["routes"]
+    data = collection.find_one({"_id": ObjectId(id)})
+    if data:
+        return models.Route.from_dict(data)
+
+def delete_route_by_id(id: str):
+    collection = db["routes"]
+    result = collection.delete_one({"_id": ObjectId(id)})
+    return result
+
+
+# SnowCondition Functions:
+def insert_snow_condition(condition: models.SnowCondition):
+    collection = db["snow_conditions"]
+    result = collection.insert_one(condition.to_dict())
+    return str(result.inserted_id)
+
+def get_snow_condition_by_road_id(road_id: str):
+    collection = db["snow_conditions"]
+    data = collection.find_one({"SnowCondition_information.road_id": road_id})
+    if data:
+        return models.SnowCondition.from_dict(data)
+
+def get_snow_condition_by_id(id: str):
+    collection = db["snow_conditions"]
+    data = collection.find_one({"_id": ObjectId(id)})
+    if data:
+        return models.SnowCondition.from_dict(data)
+
+def delete_snow_condition_by_id(id: str):
+    collection = db["snow_conditions"]
+    result = collection.delete_one({"_id": ObjectId(id)})
+    return result
