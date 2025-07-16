1f32af51f66185eb5d5aa61d961234d0c20280bc
Amir <amircan206@gmail.com>
2025-07-16 14:24:25 -0400

    system path fixes, controllers are not apropriately seen and recognized in the app.py domain

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 load_dotenv()
 
